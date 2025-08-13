5ebf18882fb84975dae345275b0693ca109e7681
BiswajitMakhal <makhalbiswajit1@gmail.com>
2025-08-14 04:50:27 +0530

    notes update

---
+++ var.js
@@ -0,0 +1,39 @@
+//-------TDZ 1 THEKE 5 PROJONTO TEMPORAL DEAD ZONE
+// JS JANE JANE JE VARIABLE ACHE KINTU VALUE DITE PARENA //
+
+// console.log(c);
+
+// let c = 15;
+
+//-----HOISTING//
+
+// var a = undefined;
+
+// console.log(a);
+
+// a = 25;
+
+//------ VAR JODI CURLY BRACESER VITORE THAKE AR KONO ROKOM FUNCTION-ER VITORE THAKE//
+//NA TAHOLEO SETA GLOBAL SCOPE//
+
+// var x = 1;  // global scope //
+// {
+//     var x = 3;//eitao global scope//
+// }
+// console.log(x);
+
+// ------LET KEYWORDS CURLY BRACES-ER RESPECT KORE
+
+// let a = 10;//global scope//
+// {
+//     let a = 20;//block scope//
+//     console.log("inside", a);
+// }
+// console.log("outside", a);
+
+
+//-------VAR DIYE VARIABLE BANALE FUNCTION SCOPE HOI, KARON VAR KEYWORDS SUDHU FUNCTION SCOPE-ER RESPECT KORE BAAKI//
+//IF OR FOR KE KONO RESPECT KORE NA//
+
+
+
