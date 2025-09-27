ba4028f85d112379934ab307158b82445b7b1246
BiswajitMakhal <makhalbiswajit1@gmail.com>
2025-09-28 03:39:27 +0530

    update

--- loop.js
+++ loop.js
@@ -55,3 +55,72 @@ do {
     count++;
 }
 while (count < 2); //condition false holeu , do-ar moddhe jei code ta ache seta akbar print hobe//
+
+
+
+
+//---------------- BREAK AND CONTINUE:
+
+//break:
+let a = 28;
+for (let a = 0; a < 41; a++) {
+    console.log(a);
+    if (a === 31) {
+        break; //break loop সম্পূর্ণভাবে বন্ধ করে দেয় যেখানেই এটি লেখা থাকে। jodi a-tar value 31 hoi tahole break hoe jabe mean theme jabe//
+    }
+
+}
+
+//continue:
+let b = 35;
+for (let b = 0; b < 41; b++) {
+    if (b === 31) {
+        continue; //continue current iteration skip করে পরের iteration এ চলে যায়।  31-skip hoe jabe.
+    }
+    console.log(b);
+}
+
+
+
+// -------------------THEORY-QUESTIONS:
+
+// ✔️1. Print numbers from 1 to 10 using a for loop. 
+for (let d = 1; d <= 10; d++) {
+    console.log(d);
+}
+
+
+
+// ✔️2. Print numbers from 10 to 1 using while loop.
+let e = 10;
+while (e > 0) {
+    console.log(e); //while loop-a first code lekha hoi then change
+    e--; //change
+}
+
+
+// ✔️3.Print even numbers from 1 to 20 using a for loop.
+
+for (let s = 1; s < 21; s++) {
+    if (s % 2 === 0) {
+        console.log(s);
+    }
+}
+
+// ✔️4.Print odd numbers from 1 to 15 using a while loop.
+
+let v = 1;
+while (v < 16) {
+    if (v % 2 === 1) {
+        console.log(v);
+    }
+    v++;
+}
+
+// ✔️5.Print the multiplication table of 5 (i.e., 5 × 1 = 5............5 × 10 = 50)
+
+let x = 1
+
+
+
+
