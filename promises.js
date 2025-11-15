d829a9fe1ac88c0be159d1fe915df049ca914725
BiswajitMakhal <makhalbiswajit1@gmail.com>
2025-11-16 01:53:07 +0530

    promises update

--- promises.js
+++ promises.js
@@ -6,25 +6,48 @@
 
 // Example of ASYNCHRONOUS:
 
-console.log("start");//synchronous code  jeta immediate run hoi
+console.log("start"); //synchronous code  jeta immediate run hoi
 
 setTimeout(() => {
-    console.log("middle");//akhane setTimeout javascript-er asynchronous function , akhane code ta call stack-a jabe na first 1 second timer end hole callback queue te jabe, then event loop check kore je call stack khali ache kina,jehutu akhane khali ache tai callback queue theke call stack-a pathai tarpor run hoi.//
+  console.log("middle"); //akhane setTimeout javascript-er asynchronous function , akhane code ta call stack-a jabe na first 1 second timer end hole callback queue te jabe, then event loop check kore je call stack khali ache kina,jehutu akhane khali ache tai callback queue theke call stack-a pathai tarpor run hoi.//
 }, 1000);
 
-console.log("end");//atao synchronous code
+console.log("end"); //atao synchronous code
 
 // OUTPUT:
 // start
 // end
 // middle
 
-
-
-
-
 //-------------- CALLBACK : AI FUNCTION TA HOLO SEI FUNCTION JETA ARAKTA FUNCTION-ER ARGUMENT HISEBE PATHANO HOI PARAMETER-A.
 
+// ---------------CALLBACK HELL:
+// Callback Hell মানে হলো — যখন অনেকগুলো callback function একটার ভিতরে আরেকটা nested হয়ে যায়, তখন কোড দেখতে খুব জটিল,
+//  লম্বা আর বোঝা কঠিন হয়ে পড়ে।
+// এমন কোডকে বলে “callback hell” বা “pyramid of doom”।
+
+doTask1(function () {
+  console.log("Task 1 done");
+
+  doTask2(function () {
+    console.log("Task 2 done");
+
+    doTask3(function () {
+      console.log("Task 3 done");
+
+      doTask4(function () {
+        console.log("Task 4 done");
+      });
+    });
+  });
+});
+
+// 👉doTask1 কাজ শেষ হলে callback কল করে
+// 👉তার ভিতরে doTask2 আবার callback নেয়
+// 👉তারপর doTask3
+// 👉তারপর doTask4
+// 👉এভাবে nested → nested → nested → বেশি গভীর হয়ে যায়
+// 👉এটাই হচ্ছে callback hell (Pyramid of Doom).
 
 // ------------STACK: STACK HOLO AKTA DATA STRUCTURE, LIFO (LAST IN FIRST OUT) METHOD-A CHOLE, JETA SOBAR SESHE DHUKCHE SETA SOBAR AAGE BER HOBE.
 
@@ -32,9 +55,6 @@ console.log("end");//atao synchronous code
 // Push B → [A, B]
 // Push C → [A, B, C]
 // Pop → C বের হবে (কারণ শেষবার ঢুকেছিলো)
-
-
-
 
 //------------- CALL STACK: Call Stack হলো JS Engine-এর ভেতরের একটি মেকানিজম যা কোড (function calls) চালাতে Stack ডাটা স্ট্রাকচার ব্যবহার করে।
 
@@ -44,11 +64,11 @@ console.log("end");//atao synchronous code
 
 // EXAMPLE:
 function one() {
-    two();
+  two();
 }
 
 function two() {
-    console.log("Hello");
+  console.log("Hello");
 }
 
 one();
@@ -67,10 +87,6 @@ one();
 
 // 👉 শেষে Call Stack ফাঁকা
 
-
-
-
-
 // --------------PROMISES: (PROMISE HOLO AKTA OBJECT, JETA BOLE DEI JE AKHON AMI RESULT DITE PARBO NA, VOBISHOT-A JODI SUCCESS HOI THEN RESULT(resolve) DEBE, NAHOLE ERROR(reject) DEBE. )
 
 // কিন্তু ভবিষ্যতে তিনটার মধ্যে একটা হবে:
@@ -81,34 +97,33 @@ one();
 
 // 3. Rejected → কাজ ব্যর্থ হলে error দেবে
 
-
-
 // PROMISE WITH VARIABLE:
 
-const myPromise = new Promise((resolve, reject) => {  //(resolve, reject) => { ... } অংশকে বলে executor function।.   resolve এবং reject এগুলো executor function এর parameters// 
-    let success = false;
-    if (success) {  //if block sudhu tokhoni run hoi jokhon condition true hoi false hole skip hoi//
-        resolve("✅Operation successful");
-    } else {
-        reject("❌Operation failed");
-    }
+const myPromise = new Promise((resolve, reject) => {
+  //(resolve, reject) => { ... } অংশকে বলে executor function।.   resolve এবং reject এগুলো executor function এর parameters//
+  let success = false;
+  if (success) {
+    //if block sudhu tokhoni run hoi jokhon condition true hoi false hole skip hoi//
+    resolve("✅Operation successful");
+  } else {
+    reject("❌Operation failed");
+  }
 });
 
 myPromise
-    .then((result) => {     //resolve hole .then cholbe, (result)--> holo parameter ar parameter hisebe sei value pabe jeta resolve-te pathano hoechilo//
-        console.log(result);
-    })
-    .catch((error) => {     //reject hole .catch cholbe// (error)--> holo parameter ar parameter hisebe sei value pabe jeta reject-te pathano hoechilo//
+  .then((result) => {
+    //resolve hole .then cholbe, (result)--> holo parameter ar parameter hisebe sei value pabe jeta resolve-te pathano hoechilo//
+    console.log(result);
+  })
+  .catch((error) => {
+    //reject hole .catch cholbe// (error)--> holo parameter ar parameter hisebe sei value pabe jeta reject-te pathano hoechilo//
 
-        console.error(error);  //kono kichu error or warning-er jonno aita use hoi//
-
-    })
-    .finally(() => {   //.finally sob somoi cholbe ,resolve hok or reject hok na keno, kichu kichu kaaj thake jeta sofol or bartho hok na keno korte hobei, jemon loading screen off kora , database connection close kora , memory free kora etc.//
-        console.log("Promise completed");
-    });
-
-
-
+    console.error(error); //kono kichu error or warning-er jonno aita use hoi//
+  })
+  .finally(() => {
+    //.finally sob somoi cholbe ,resolve hok or reject hok na keno, kichu kichu kaaj thake jeta sofol or bartho hok na keno korte hobei, jemon loading screen off kora , database connection close kora , memory free kora etc.//
+    console.log("Promise completed");
+  });
 
 // ------------EXECUTOR FUNCTION:
 
@@ -119,111 +134,94 @@ myPromise
 
 // EXAMPLE:
 const promise = new Promise(function executor(resolve, reject) {
-
-    // এখানে কাজ লেখা হবে
-
+  // এখানে কাজ লেখা হবে
 });
-
-
-
 
 // PROMISE WITH ASYNCHRONOUS OR FUNCTION :
 
 // EX:1
 function fetchData() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {    //setTimeout holo js-ar built-in function, jeta bole ai function ke akhuni use korona khanikhon por koro//
-            let dataFound = true;
-            if (dataFound) {
-                resolve({ id: 1, name: "Pizza" });
-            } else {
-                reject("Data not found");
-            }
-        }, 2000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //setTimeout holo js-ar built-in function, jeta bole ai function ke akhuni use korona khanikhon por koro//
+      let dataFound = true;
+      if (dataFound) {
+        resolve({ id: 1, name: "Pizza" });
+      } else {
+        reject("Data not found");
+      }
+    }, 2000);
+  });
 }
 
 fetchData()
-    .then((data) => console.log("Received:", data))  //aita single line arrow function use hoeche tai arrow-tar por curly braces use hoini
-    .catch((err) => console.error("Error:", err));
-
-
+  .then((data) => console.log("Received:", data)) //aita single line arrow function use hoeche tai arrow-tar por curly braces use hoini
+  .catch((err) => console.error("Error:", err));
 
 // EX:2
 let ExPromise = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let name = true;
-            if (name) {
-                resolve("Name: Biswajit");
-            }
-            else {
-                reject("Data is not found");
-            }
-        }, 4000);
-    });
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let name = true;
+      if (name) {
+        resolve("Name: Biswajit");
+      } else {
+        reject("Data is not found");
+      }
+    }, 4000);
+  });
+};
 
 ExPromise()
-    .then((result) => {
-        console.log(result);
-        return ("Data is found")  //.then-ar moddhe return korale seta porer .then moddhe deoa parameter-a jai //
-    })
-    .then((re) => {
-        console.log(re);
-    })
-    .catch(error => {  //akhane sudhu akta parameter ache bole bracket deoa hoini, multiple parameter thakle dite hoto , akhane na dileo chole//
-        console.log(error);
-    })
-    .finally(() => {
-        console.log("Promise completed wihout async and await");
-    })
-
-
+  .then((result) => {
+    console.log(result);
+    return "Data is found"; //.then-ar moddhe return korale seta porer .then moddhe deoa parameter-a jai //
+  })
+  .then((re) => {
+    console.log(re);
+  })
+  .catch((error) => {
+    //akhane sudhu akta parameter ache bole bracket deoa hoini, multiple parameter thakle dite hoto , akhane na dileo chole//
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("Promise completed wihout async and await");
+  });
 
 //------------ ASYNC- AWAIT
 
 // ASYNC: JavaScript এ async হলো একটা কীওয়ার্ড, যেটা আমরা function এর আগে লিখি ,এটা বোঝায় যে ওই function টা asynchronous ভাবে কাজ করবে এবং সবসময় Promise return করবে। ।
-
-
 
 // AWAIT: await হলো JavaScript এর একটা কীওয়ার্ড যেটা শুধু async function এর ভেতরে ব্যবহার করা যায়।
 // এর কাজ হলো —
 
 // 👉 কোনো Promise resolve (অথবা reject) না হওয়া পর্যন্ত কোড execution কে থামিয়ে রাখা।
 
-
-
 // TRY AND CATCH : JavaScript-এ try...catch হলো error (ভুল) ধরার ব্যবস্থা। মানে, তুমি এমন কোড চালাতে পারো যেটাতে ভুল হবার সম্ভাবনা আছে। যদি ভুল হয় → প্রোগ্রাম বন্ধ হবে না, বরং সেটা catch ব্লকে ধরা পড়বে।
 
-
 let ExAsync = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let Data = true;
-            if (Data) {
-                resolve("Data: I am an async and await's example")
-            }
-            else {
-                reject("Data is not found")
-            }
-        }, 5000)
-    })
-}
-
-
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let Data = true;
+      if (Data) {
+        resolve("Data: I am an async and await's example");
+      } else {
+        reject("Data is not found");
+      }
+    }, 5000);
+  });
+};
 
 let Show = async () => {
-    try {
-        let ResolveValue = await ExAsync(); //akhane jodi variable na dao tahole resolve-r data ta akhane transfer hobe na, tai akahne ResolveValue bole variable naoa holo//
-        console.log(ResolveValue);
+  try {
+    let ResolveValue = await ExAsync(); //akhane jodi variable na dao tahole resolve-r data ta akhane transfer hobe na, tai akahne ResolveValue bole variable naoa holo//
+    console.log(ResolveValue);
 
-        let user = "AsyncData is found"
-        console.log(user);
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+    let user = "AsyncData is found";
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 Show();
